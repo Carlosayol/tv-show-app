d121aa1b0e6b0b04fe2a39b2e32f8ac2feb6aff1
Carlosayol <Carlosaolar@gmail.com>
2023-03-03 14:31:47 -0500

    feat: setup env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,6 @@
-export const API_KEY = "83ffbd0e76efc8ddb90a0317edd5a58a";
+import { APP_KEY } from '@env'
+
+export const API_KEY = APP_KEY;
 export const BASE_URL = "https://api.themoviedb.org/3/";
 export const POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w200";
 export const POSTER_IMAGE_URL_B = "https://image.tmdb.org/t/p/w500";
